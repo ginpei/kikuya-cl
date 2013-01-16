3c79ea0ff0061cf6729c1ecffcfac8912aae52eb
ginpei <ginpei@debian.(none)>
2013-01-17 01:15:33 +0900

    user view's interactions

--- spec/view/UserView.js
+++ spec/view/UserView.js
@@ -1,14 +1,78 @@
 define([
-	'view/UserView'
-], function(UserView) {
+	'app',
+	'view/UserView',
+	// 'model/User'
+], function(app, UserView, User) {
+	var User = app.newModel();
 	describe('UserView', function() {
 		var view;
 		beforeEach(function() {
-			view = new UserView();
+			view = new UserView(new User());
+			view.render();
 		});
 
-		it('is a constructor', function() {
-			expect(view instanceof UserView).toBeTruthy();
+		it('sets its element position', function() {
+			view.setPosition(123, 234);
+			expect(parseInt(view.$el.css('left'))).toBe(123);
+			expect(parseInt(view.$el.css('top'))).toBe(234);
+		});
+
+		describe('touches', function() {
+			var touchStartEvent, touchMoveEvent;
+			beforeEach(function() {
+				touchStartEvent = jQuery.Event('touchstart');
+				touchStartEvent.originalEvent = { touches: [{}] };
+
+				touchMoveEvent = jQuery.Event('touchmove');
+				touchMoveEvent.originalEvent = { touches: [{}] };
+			});
+
+			it('starts moving', function() {
+				expect(view.isMoving()).toBeFalsy();
+
+				view.$el.trigger(touchStartEvent);
+				expect(view.isMoving()).toBeTruthy();
+			});
+
+			it('stop moving', function() {
+				view.$el.trigger(touchStartEvent);
+				expect(view.isMoving()).toBeTruthy();
+
+				view.$el.trigger('touchstop', { });
+				expect(view.isMoving()).toBeFalsy();
+			});
+
+			it('memories touch position on touchstart', function() {
+				touchStartEvent.originalEvent = {
+					touches: [{ pageX:123, pageY:234 }]
+				};
+				view.$el.trigger(touchStartEvent);
+				expect(view._posTouch[0]).toBe(123);
+				expect(view._posTouch[1]).toBe(234);
+			});
+
+			it('memories element position on touchstart', function() {
+				view.setPosition(100, 101);
+				view.$el.trigger(touchStartEvent);
+				expect(view._posElement[0]).toBe(100);
+				expect(view._posElement[1]).toBe(101);
+			});
+
+			it('updates element position by touch position', function() {
+				view.setPosition(100, 101);
+				touchStartEvent.originalEvent = {
+					touches: [{ pageX:123, pageY:234 }]
+				};
+				view.$el.trigger(touchStartEvent);
+
+				touchMoveEvent.originalEvent = {
+					touches: [{ pageX:124, pageY:235 }]
+				};
+				view.$el.trigger(touchMoveEvent);
+
+				expect(parseInt(view.$el.css('left'))).toBe(101);
+				expect(parseInt(view.$el.css('top'))).toBe(102);
+			});
 		});
 	});
 });
--- src/view/UserView.js
+++ src/view/UserView.js
@@ -5,6 +5,12 @@ define([
 		tagName: 'li',
 		className: 'user',
 
+		events: {
+			'touchmove': 'move',
+			'touchstart': 'startMoving',
+			'touchstop': 'stopMoving'
+		},
+
 		initialize: function(user) {
 			this.model = user;
 		},
@@ -12,9 +18,57 @@ define([
 		render: function() {
 			var id = this.cid;
 			this.$el
+				.css('position', 'absolute')  // TODO: move to CSS
 				.text(this.model.get('name') || 'New User#'+this.model.cid);
+			this.setPosition(0, 0);
 
 			return this;
+		},
+
+		setPosition: function(left, top) {
+			this.$el.css({ left:left, top:top });
+		},
+
+		/**
+		 * On touchstart.
+		 */
+		startMoving: function(event) {
+			this._moving = true;
+
+			var touch = event.originalEvent.touches[0];
+			this._posTouch = [touch.pageX, touch.pageY];
+
+			var $el = this.$el;
+			this._posElement = [
+				parseInt($el.css('left')),
+				parseInt($el.css('top'))
+			];
+		},
+
+		/**
+		 * On touchstop.
+		 */
+		stopMoving: function(event) {
+			this._moving = false;
+		},
+
+		/**
+		 * On touchmove.
+		 */
+		move: function(event) {
+			var touch = event.originalEvent.touches[0];
+			var posTouchStarted = this._posTouch;
+			var posElement = this._posElement;
+			var left = posElement[0] + touch.pageX - posTouchStarted[0];
+			var top  = posElement[1] + touch.pageY - posTouchStarted[1];
+			this.setPosition(left, top);
+		},
+
+		/**
+		 * @returns {Boolean}
+		 */
+		isMoving: function() {
+			return this._moving;
 		}
 	});
 });
